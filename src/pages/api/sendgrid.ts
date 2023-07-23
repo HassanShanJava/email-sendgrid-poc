38b3e61f7baa032fb298271fac2fb222219e1b44
HassanShanJava <hassanshan675@gmail.com>
2023-07-24 04:21:00 +0500

    built api and tested it

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -1,3 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 require("dotenv").config()
 
-const sgMail=require("@sendgrid/mail")
+const sgMail = require("@sendgrid/mail")
+
+const { SG_API_KEY, FROM_EMAIL, TO_EMAIL } = process.env
+
+sgMail.setApiKey(SG_API_KEY)
+
+
+export default async function sendEmail(req: NextApiRequest, res: NextApiResponse){
+  const { message, name, email } = req.body
+
+  const msg = {
+    to: email,
+    from: FROM_EMAIL,
+    subject: "Nextjs contact form",
+    html: `<p><strong>Form: ${name}</strong></p> 
+          <br/>
+          <p><strong>Email: ${email}</strong></p>
+          <br/>
+          <p><strong>Text: ${message}</strong></p>
+          `,
+  };
+
+  try {
+    await sgMail.send(msg);
+    res.json({success:true})
+    console.log(`Email sent to ${email}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,38 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e: any) =>
+    setValues({ ...values, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault()
+    console.log("inside")
+    try{
+      await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/sendgrid`,{
+        method:"POST",
+        headers:{
+          'Content-Type':"application/json"
+        },
+        body:JSON.stringify(values),
+        // mode:"no-cors"
+      })
+    }catch(err){
+      console.log({err},'err')
+    }
+  };
+
+  const { name, email, message } = values;
   return (
     <>
       <Head>
@@ -15,28 +43,47 @@ export default function Home() {
         <div className="flex flex-col justify-start ">
           <p className="font-semibold">Contact form</p>
           {/* name */}
-          {/* <form></form> */}
-          <input
-            type="name"
-            placeholder="Enter name"
+          <form onSubmit={handleSubmit}>
+            <input
+              type="name"
+              name="name"
+              onChange={handleChange}
+              value={name}
+              placeholder="Enter name"
+              className="p-2 my-2 border border-black w-full rounded-md"
+            />
+            {/* email */}
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={email}
+              placeholder="Enter email"
+              className="p-2 my-2 border border-black w-full rounded-md"
+            />
+            {/* subject */}
+            {/* <input
+            type="text"
+            placeholder="Subject"
             className="p-2 my-2 border border-black w-full rounded-md"
-          />
-          {/* email */}
-          <input
-            type="email"
-            placeholder="Enter email"
-            className="p-2 my-2 border border-black w-full rounded-md"
-          />
-          {/* text */}
-          <textarea
-            placeholder="Enter msg"
-            className="p-2 my-2 border border-black rounded-md"
-            rows={5}
-          />
+          /> */}
+            {/* text */}
+            <textarea
+              name="message"
+              value={message}
+              onChange={handleChange}
+              placeholder="Enter msg"
+              className="p-2 my-2 border border-black rounded-md w-full"
+              rows={5}
+            />
 
-          <button className="bg-blue-950 text-white font-semibold text-center w-full my-2 p-2 rounded-md">
-            Send
-          </button>
+            <button
+              type="submit"
+              className="bg-blue-950 text-white font-semibold text-center w-full my-2 p-2 rounded-md"
+            >
+              Send
+            </button>
+          </form>
         </div>
       </div>
     </>
