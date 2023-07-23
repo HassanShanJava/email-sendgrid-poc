aaf73b2f7b979a249a9cbd59f01718117608ccdc
HassanShanJava <hassanshan675@gmail.com>
2023-07-23 07:23:41 +0500

    added title and gap to heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Sendgrid Test</title>
+      </Head>
       <div className="max-w-[500px] m-auto p-5">
-        <h1 className="font-bold text-center text-2xl0">Sendgrid Test</h1>
+        <h1 className="font-bold text-center text-2xl my-5">Sendgrid Test</h1>
         <div className="flex flex-col justify-start ">
           <p className="font-semibold">Contact form</p>
           {/* name */}
@@ -30,7 +34,9 @@ export default function Home() {
             rows={5}
           />
 
-          <button className="bg-blue-950 text-white font-semibold text-center w-full my-2 p-2 rounded-md">Send</button>
+          <button className="bg-blue-950 text-white font-semibold text-center w-full my-2 p-2 rounded-md">
+            Send
+          </button>
         </div>
       </div>
     </>
